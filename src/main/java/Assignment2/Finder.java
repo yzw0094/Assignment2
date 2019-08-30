9c2d5a25b5408b11cf54ffe6776d4fba342bcdeb
yzw0094 <loverstarer@gmail.com>
2019-08-30 14:04:33 -0500

    Initial commit

---
+++ src/main/java/Assignment2/Finder.java
@@ -0,0 +1,33 @@
+package Assignment2;
+/**
+ COMP 3700
+ Assignment 2
+ Yannan Wang
+ 08/28/2019
+ */
+public class Finder {
+    public static Integer findMax(int[] intArray) {
+        if (intArray == null || intArray.length == 0) {
+            return null;
+        }
+        int mx = intArray[0];
+        for (int a = 0; a < intArray.length; a++) {
+            if (mx < intArray[a]) {
+                mx = intArray[a];
+            }
+        }
+        return mx;
+    }
+
+    public static Integer findMin(int[] intArray) {
+        if (intArray == null || intArray.length ==0) {
+            return null;
+        }
+        int mn = intArray[0];
+        for (int b = 0; b < intArray.length; b++) {
+            if (mn > intArray[b])
+                mn = intArray[b];
+        }
+        return mn;
+    }
+}
